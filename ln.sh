f5909f2aaa312a1fecbf9099716aa599ff34002d
hitalin <hitalin@users.noreply.github.com>
2017-04-08 12:59:44 +0900

    add ln.sh

---
+++ ln.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim/colors ~/.vim
+ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ls -s ~/.dotfiles/gdb/.gdbinit ~/.gdbinit
