f919bf8f6abe344d172a3d5f333b5e67d6f00d22
hitalin <hitalin@users.noreply.github.com>
2017-06-27 04:48:45 +0900

    fix shebang

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 
 if [ ! -f ~/.vim/colors/hybrid.vim ]; then
     git clone https://github.com/w0ng/vim-hybrid.git ~/.vim/
-    mv ~/.vim/vim-hybrid/colors ../
-    rm -rf ~/.vim/vim-hybrid
+    rm ~/.vim/README.md 
 fi
 
 if [ ! -d ~/peda/ ]; then
     git clone https://github.com/longld/peda.git ~/peda
     echo "source ~/peda/peda.py" >> ~/.gdbinit
 fi
+
+chsh -s $(which zsh)
