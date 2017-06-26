bcdf7a40f918f8036b46f420a8a240aefd2d215a
hitalin <hitalin@users.noreply.github.com>
2017-06-27 04:48:21 +0900

    update ln.sh

--- ln.sh
+++ ln.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
-ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim/colors ~/.vim
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ls -s ~/.dotfiles/gdb/.gdbinit ~/.gdbinit
+
+if [ ! -d ~/.vim/rc/ ]; then
+    mkdir -p ~/.vim/rc
+    ln -Fis $SCRIPT_DIR/dein.toml ~/.vim/rc/
+    ln -Fis $SCRIPT_DIR/dein_lazy.toml ~/.vim/rc/
+fi
+
+if [ ! -z $TMUX_ENV ]; then
+    echo "set-option -g prefix C-t" > ~~~tmp
+    echo ".tmux.conf setting for remote."
+else
+    echo "set-option -g prefix C-g" > ~~~tmp
+    echo ".tmux.conf setting for local."
+fi  
+cat ~~~tmp tmux.conf > ~~~tmux.conf
+mv ~~~tmux.conf .tmux.conf
+rm -f ~~~tmp
+
+for file in `find $HOME/dotfiles -name '.*' | grep -v 'dotfiles/.git$' | perl -nle 'm!dotfiles/(.+)$! and print $1'`; do
+    ln -s -f $HOME/dotfiles/$file $HOME/$file
+done
