33fd5bd151ae646c5e4fc5297131ecbed4b1c3f4
hitalin <hitalin@users.noreply.github.com>
2017-06-27 03:52:22 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,12 @@
-#!/bin/sh
-
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+#!/bin/bash
 
 if [ ! -f ~/.vim/colors/hybrid.vim ]; then
-    mkdir -p ~/.vim/colors/
-    git clone https://github.com/w0ng/vim-hybrid.git $SCRIPT_DIR/hybrid.vim
-    mv $SCRIPT_DIR/.vim/colors/hybrid.vim ~/.vim/colors/hybrid.vim
-    rm -rf $SCRIPT_DIR/hybrid.vim
+    git clone https://github.com/w0ng/vim-hybrid.git ~/.vim/
+    mv ~/.vim/vim-hybrid/colors ../
+    rm -rf ~/.vim/vim-hybrid
 fi
 
 if [ ! -d ~/peda/ ]; then
     git clone https://github.com/longld/peda.git ~/peda
     echo "source ~/peda/peda.py" >> ~/.gdbinit
 fi
-
-echo "Finished!"
