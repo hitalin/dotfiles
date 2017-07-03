bb161c47e5252548640ede6dd27e2d871636f6fa
hitalin <hitalin@users.noreply.github.com>
2017-07-04 03:17:30 +0900

    set_py2.sh: fix it a bit

--- python/set_py2.sh
+++ python/set_py2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Updates
-sudo pacman -Syy
-sudo pacman -S --noconfirm python2-pip
+# Set pip2 
+pacman -Syy
+pacman -S --noconfirm python2-pip
 
 # Install virtualenvwrapper
-sudo pip2 install virtualenvwrapper
+pip2 install virtualenvwrapper
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
 source /usr/bin/virtualenvwrapper.sh 
