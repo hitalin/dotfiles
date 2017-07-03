df139c25e3eb7cbde2f33625e0fab9339e0f980b
hitalin <hitalin@users.noreply.github.com>
2017-07-04 03:01:52 +0900

    set_py2.sh: fix pip to pip2

--- python/set_py2.sh
+++ python/set_py2.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-HOMEDIR=/home/taka
 
 # Updates
 sudo pacman -Syy
 sudo pacman -S --noconfirm python2-pip
 
 # Install virtualenvwrapper
-sudo pip install virtualenvwrapper
+sudo pip2 install virtualenvwrapper
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
 source /usr/bin/virtualenvwrapper.sh 
