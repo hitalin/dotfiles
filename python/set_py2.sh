8b6b673f4a9a8a47b71d6d54bee63127a0df06dc
hitalin <hitalin@users.noreply.github.com>
2017-11-23 08:27:07 +0900

    fix set_py2.sh for arch linux

--- python/set_py2.sh
+++ python/set_py2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Set pip2 
-pacman -Syy
-pacman -S --noconfirm python2-pip
+sudo pacman -Syy
+sudo pacman -S --noconfirm python2-pip
 
 # Install virtualenvwrapper
-pip2 install virtualenvwrapper
+sudo pip2 install virtualenvwrapper
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
 source /usr/bin/virtualenvwrapper.sh 
