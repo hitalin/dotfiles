cfb0b67c7f7f58da43f6488d62933061efcce8c5
hitalin <hitalin@users.noreply.github.com>
2018-02-04 00:59:52 +0900

    fix qemu/download.sh: comment out

--- qemu/download.sh
+++ qemu/download.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-if is_setup 'ARMLE Debian Wheezy image'; then
-  echo 'Downloading & setting ARMLE Debian Wheezy...'
+# if is_setup 'ARMLE Debian Wheezy image'; then
+#   echo 'Downloading & setting ARMLE Debian Wheezy...'
   mkdir -p ~/qcow2/armle
   wget https://people.debian.org/~aurel32/qemu/armel/debian_wheezy_armel_standard.qcow2 -O ~/qcow2/armle/debian_wheezy_armel_standard.qcow2
   wget https://people.debian.org/~aurel32/qemu/armel/initrd.img-3.2.0-4-versatile -O ~/qcow2/armle/initrd.img-3.2.0-4-versatile
   wget https://people.debian.org/~aurel32/qemu/armel/vmlinuz-3.2.0-4-versatile -O ~/qcow2/armle/vmlinuz-3.2.0-4-versatile
-fi
+# fi
 
-if is_setup 'MIPSBE Debian Wheezy image'; then
-  echo 'Downloading & setting MIPSBE Debian Wheezy...'
-  mkdir -p ~/qcow2/mipsbe
-  wget https://people.debian.org/~aurel32/qemu/mips/debian_wheezy_mips_standard.qcow2 -O ~/qcow2/mipsbe/debian_wheezy_mips_standard.qcow2
-  wget https://people.debian.org/~aurel32/qemu/mips/vmlinux-3.2.0-4-4kc-malta -O ~/qcow2/mipsbe/vmlinux-3.2.0-4-4kc-malta
-fi
-
-if is_setup 'MIPSLE Debian Wheezy image'; then
-  echo 'Downloading & setting MIPSLE Debian Wheezy...'
-  mkdir -p ~/qcow2/mipsle
-  wget https://people.debian.org/~aurel32/qemu/mipsel/debian_wheezy_mipsel_standard.qcow2 -O ~/qcow2/mipsle/debian_wheezy_mipsel_standard.qcow2
-  wget https://people.debian.org/~aurel32/qemu/mipsel/vmlinux-3.2.0-4-4kc-malta -O ~/qcow2/mipsle/vmlinux-3.2.0-4-4kc-malta
-fi
-
-if is_setup 'AArch64 Debian Wheezy image'; then
-  echo 'Downloading & setting AArch64 Ubuntu Trusty...'
-  mkdir -p ~/qcow2/aarch64
-  wget https://cloud-images.ubuntu.com/trusty/current/unpacked/trusty-server-cloudimg-arm64-vmlinuz-generic -O ~/qcow2/aarch64/trusty-server-cloudimg-arm64-vmlinuz-generic
-  wget https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-arm64-disk1.img -O ~/qcow2/aarch64/trusty-server-cloudimg-arm64-disk1.img
-fi
+# if is_setup 'MIPSBE Debian Wheezy image'; then
+#   echo 'Downloading & setting MIPSBE Debian Wheezy...'
+#   mkdir -p ~/qcow2/mipsbe
+#   wget https://people.debian.org/~aurel32/qemu/mips/debian_wheezy_mips_standard.qcow2 -O ~/qcow2/mipsbe/debian_wheezy_mips_standard.qcow2
+#   wget https://people.debian.org/~aurel32/qemu/mips/vmlinux-3.2.0-4-4kc-malta -O ~/qcow2/mipsbe/vmlinux-3.2.0-4-4kc-malta
+# fi
+# 
+# if is_setup 'MIPSLE Debian Wheezy image'; then
+#   echo 'Downloading & setting MIPSLE Debian Wheezy...'
+#   mkdir -p ~/qcow2/mipsle
+#   wget https://people.debian.org/~aurel32/qemu/mipsel/debian_wheezy_mipsel_standard.qcow2 -O ~/qcow2/mipsle/debian_wheezy_mipsel_standard.qcow2
+#   wget https://people.debian.org/~aurel32/qemu/mipsel/vmlinux-3.2.0-4-4kc-malta -O ~/qcow2/mipsle/vmlinux-3.2.0-4-4kc-malta
+# fi
+# 
+# if is_setup 'AArch64 Debian Wheezy image'; then
+#   echo 'Downloading & setting AArch64 Ubuntu Trusty...'
+#   mkdir -p ~/qcow2/aarch64
+#   wget https://cloud-images.ubuntu.com/trusty/current/unpacked/trusty-server-cloudimg-arm64-vmlinuz-generic -O ~/qcow2/aarch64/trusty-server-cloudimg-arm64-vmlinuz-generic
+#   wget https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-arm64-disk1.img -O ~/qcow2/aarch64/trusty-server-cloudimg-arm64-disk1.img
+# fi
 
 echo 'Done'
