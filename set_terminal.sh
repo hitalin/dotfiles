258ba66acb339e6acdee8b81832db62d0a245d20
hitalin <hitalin@users.noreply.github.com>
2018-06-19 02:57:35 +0900

    separate setup.sh to set_terminal.sh and ln.sh

--- set_terminal.sh
+++ set_terminal.sh
@@ -2,10 +2,10 @@
 set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-#if [ ! -f ~/.vim/colors/hybrid.vim ]; then
-#    git clone https://github.com/w0ng/vim-hybrid.git ~/.vim/
-#    rm ~/.vim/README.md 
-#fi
+if [ ! -f ~/.vim/colors/hybrid.vim ]; then
+    git clone https://github.com/w0ng/vim-hybrid.git ~/.vim/
+    rm ~/.vim/README.md 
+fi
 
 if [ ! -d ~/.vim/rc/ ]; then
     mkdir -p ~/.vim/rc
@@ -24,10 +24,6 @@ cat ~~~tmp .tmux.conf > ~~~tmux.conf
 mv ~~~tmux.conf .tmux.conf
 rm -f ~~~tmp
 
-for file in `find $HOME/dotfiles -name '.*' | grep -v 'dotfiles/.git$' | perl -nle 'm!dotfiles/(.+)$! and print $1'`; do
-    ln -s -f $HOME/dotfiles/$file $HOME/$file
-done
-
-#chsh -s $(which zsh)
+chsh -s $(which zsh)
 
 echo "Finished!"
