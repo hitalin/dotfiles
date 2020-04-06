96fcd3cd8f41a23a9c4550d160b482050e7739bd
hitalin <hitalin@users.noreply.github.com>
2020-04-06 15:43:39 +0900

    remove orgdrill

--- emacs/packages.el
+++ emacs/packages.el
@@ -5,6 +5,5 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
-(package! org-drill)
 (package! ox-gfm)
 (package! sourcetrail)
