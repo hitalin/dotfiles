3dd912cb4df738fdfd77d0186d1c238d2aa5daa0
hitalin <hitalin@users.noreply.github.com>
2020-04-08 23:30:18 +0900

    fix set.sh to install minpac for vim

--- set.sh
+++ set.sh
@@ -7,20 +7,18 @@ if [ ! -f ~/.zsh/pure/pure.zsh ]; then
     git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure/"
 fi
 
+if [ ! -f ~/.vim/pack/minpac ]; then
+    git clone https://github.com/k-takata/minpac.git "$HOME/.vim/pack/minpac/opt/minpac"
+fi
+
 if [ ! -f ~/.vim/colors/molokai.vim ]; then
     mkdier -o "$HOME/.zsh/colors"
     git clone https://github.com/tomasr/molokai.git "$HOME/.vim/colors/"
 fi
 
-if [ ! -d ~/.vim/rc/ ]; then
-    mkdir -p ~/.vim/rc
-    ln -Fis $SCRIPT_DIR/vim/rc/dein.toml ~/.vim/rc/
-    ln -Fis $SCRIPT_DIR/vim/rc/dein_lazy.toml ~/.vim/rc/
-fi
-
 if [ ! -f ~/.emacs.d/ ]; then
     mkdier -o "$HOME/.emacs.d"
-    git clone https://github.com/hlissner/doom-emacs "$HOME/.emacs.d" 
+    git clone https://github.com/hlissner/doom-emacs "$HOME/.emacs.d"
 fi
 
 chsh -s $(which zsh)
