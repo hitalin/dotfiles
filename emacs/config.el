313d575ecb0de6c094aa629768c6828d303b6d22
hitalin <hitalin@users.noreply.github.com>
2020-04-10 14:15:02 +0900

    move setting from ~/.emacs.d to here by 'opam user-setup install'

--- emacs/config.el
+++ emacs/config.el
@@ -25,3 +25,7 @@
 ;; org-agenda
 (setq org-agenda-files (list "~/orgfiles"))
 (setq org-log-done 'time)
+
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/dotfiles/emacs/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
