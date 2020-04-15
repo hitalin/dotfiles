1d1afe0e6cd32044af19a06e881c124dae3c5a3b
hitalin <hitalin@users.noreply.github.com>
2020-04-15 17:14:54 +0900

    fix set.sh

--- set.sh
+++ set.sh
@@ -2,21 +2,22 @@
 set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-if [ ! -f ~/.vim/rc ]; then
+if [ ! -f ~/.vim ]; then
+    mkdir -p ~/.vim/
+    git clone https://github.com/tomasr/molokai "$HOME/.vim"
     mkdir -p ~/.vim/rc
     ln -Fis $SCRIPT_DIR/vim/rc/dein.toml ~/.vim/rc/
     ln -Fis $SCRIPT_DIR/vim/rc/dein_lazy.toml ~/.vim/rc/
-    git clone https://github.com/tomasr/molokai "$HOME/.vim"
 fi
 
 if [ ! -f ~/.emacs.d/ ]; then
+    mkdir -p "$HOME/.emacs.d"
+    git clone https://github.com/hlissner/doom-emacs "$HOME/.emacs.d"
     mkdir -p ~/.doom.d
     ln -Fis $SCRIPT_DIR/emacs/config.el ~/.doom.d/config.el
     ln -Fis $SCRIPT_DIR/emacs/early-init.el ~/.doom.d/early-init.el
     ln -Fis $SCRIPT_DIR/emacs/init.el ~/.doom.d/init.el
     ln -Fis $SCRIPT_DIR/emacs/packages.el ~/.doom.d/packages.el
-    mkdir -p "$HOME/.emacs.d"
-    git clone https://github.com/hlissner/doom-emacs "$HOME/.emacs.d"
 fi
 
 chsh -s $(which zsh)
