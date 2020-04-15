74ce91fb5106a86f3c826776bd1bff61283b1acb
hitalin <hitalin@users.noreply.github.com>
2020-04-15 17:24:52 +0900

    rm README of molokai

--- set.sh
+++ set.sh
@@ -5,6 +5,8 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 if [ ! -f ~/.vim ]; then
     mkdir -p ~/.vim/
     git clone https://github.com/tomasr/molokai ~/.vim
+    rm ~/.vim/LICENCE.md
+    rm ~/.vim/README.md
     mkdir -p ~/.vim/rc
     ln -Fis $SCRIPT_DIR/vim/rc/dein.toml ~/.vim/rc/
     ln -Fis $SCRIPT_DIR/vim/rc/dein_lazy.toml ~/.vim/rc/
