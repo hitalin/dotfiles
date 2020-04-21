395c89086c63420db518acaa4dc5056ee5ea6752
hitalin <hitalin@users.noreply.github.com>
2020-04-21 23:29:22 +0900

    add symlink for neovim

--- set.sh
+++ set.sh
@@ -2,14 +2,21 @@
 set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+if [ ! -f ~/.config/nvim/ ]; then
+  mkdir -p ~/.config/nvim/
+  ln -Fis $SCRIPT_DIR/.vimrc ~/.config/nvim/init.vim
+  mkdir -p ~/.config/nvim/colors
+  ln -Fis ~/.vim/dein/repos/github.com/flazz/vim-colorschemes/colors/molokai.vim ~/.config/nvim/colors/molokai.vim
+fi
+
 if [ ! -f ~/.emacs.d/ ]; then
-    mkdir -p ~/.emacs.d
-    git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
-    mkdir -p ~/.doom.d
-    ln -Fis $SCRIPT_DIR/emacs/config.el ~/.doom.d/config.el
-    ln -Fis $SCRIPT_DIR/emacs/early-init.el ~/.doom.d/early-init.el
-    ln -Fis $SCRIPT_DIR/emacs/init.el ~/.doom.d/init.el
-    ln -Fis $SCRIPT_DIR/emacs/packages.el ~/.doom.d/packages.el
+  mkdir -p ~/.emacs.d
+  git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+  mkdir -p ~/.doom.d
+  ln -Fis $SCRIPT_DIR/emacs/config.el ~/.doom.d/config.el
+  ln -Fis $SCRIPT_DIR/emacs/early-init.el ~/.doom.d/early-init.el
+  ln -Fis $SCRIPT_DIR/emacs/init.el ~/.doom.d/init.el
+  ln -Fis $SCRIPT_DIR/emacs/packages.el ~/.doom.d/packages.el
 fi
 
 chsh -s $(which zsh)
