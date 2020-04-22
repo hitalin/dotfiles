a9b343efaf79299f2723990cb7a4fd4fb3add04b
hitalin <hitalin@users.noreply.github.com>
2020-04-22 12:25:50 +0900

    use fcitx.el in emacs

--- emacs/packages.el
+++ emacs/packages.el
@@ -5,6 +5,8 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
+(package! fcitx)
+
 (package! ox-gfm)
 
 (package! lsp-ocaml)
