a5994d349995a3d14a62c37a0534272f822ece2d
hitalin <hitalin@users.noreply.github.com>
2020-04-25 03:27:42 +0900

    fix

--- set.sh
+++ set.sh
@@ -6,7 +6,6 @@ if [ ! -f ~/.config/nvim/ ]; then
   pip3 install --user pynvim neovim-remote
   mkdir -p ~/.config/nvim/
   ln -Fis $SCRIPT_DIR/.vimrc ~/.config/nvim/init.vim
-  ln -Fis $SCRIPT_DIR/vim/coc-settings.json ~/.vim/coc-settings.json
   mkdir -p ~/.config/nvim/colors
   ln -Fis ~/.vim/dein/repos/github.com/flazz/vim-colorschemes/colors/molokai.vim ~/.config/nvim/colors/molokai.vim
 fi
