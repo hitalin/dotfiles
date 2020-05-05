d6b8765ed5b2109d7470d61d843e353ba6433562
hitalin <hitalin@users.noreply.github.com>
2020-05-05 23:54:48 +0900

    fuck

--- emacs/config.el
+++ emacs/config.el
@@ -44,8 +44,7 @@
 (global-set-key (kbd "C-c y") 'yank-from-x-clipboard)
 
 ;; indent
-(setq-default c-basic-offset 2
-              tab-width 2
+(setq-default tab-width 2
               indent-tabs-mode nil)
 
 ; org-mode
@@ -53,26 +52,28 @@
   ;; set org file directory
   (setq org-directory "~/orgfiles/")
   ;; set agenda files
-  (setq org-agenda-files (list "~/orgfiles/TASK.org"))
+  (setq org-agenda-files (list "~/orgfiles/INBOX.org"
+                               "~/orgfiles/TASK.org"
+                               "~/orgfiles/JOURNAL.org"))
   ;; set task states
   (setq org-todo-keywords
         (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-                (sequence "WAIT(w)"  "|" "SOMEDAY(o)" "KILL(k)"))))
+                (sequence "WAITING(w)"  "|" "SOMEDAY(o)" "CANCELLED(c)"))))
   (setq org-todo-keyword-faces
         (quote (("TODO" :foreground "#cc6666" :weight bold)
                 ("NEXT" :foreground "#8abeb7" :weight bold)
                 ("DONE" :foreground "#b5bd68" :weight bold)
-                ("WAIT" :foreground "#de935f" :weight bold)
+                ("WAITING" :foreground "#de935f" :weight bold)
                 ("SOMEDAY" :foreground "#b294bb" :weight bold)
-                ("KILL" :foreground "#f0c674" :weight bold :strike-through t))))
+                ("CANCELLED" :foreground "#f0c674" :weight bold :strike-through t))))
   ;; trigger task states
   (setq org-todo-state-tags-triggers
-        (quote (("TODO" ("WAIT") ("KILL"))
-                ("NEXT" ("WAIT") ("KILL"))
-                ("DONE" ("WAIT") ("KILL"))
-                ("WAIT" ("WAIT" . t))
-                ("SOMEDAY" ("WAIT", t))
-                ("KILL" ("KILL" . t))
+        (quote (("TODO" ("WAITING") ("CANCELLED"))
+                ("NEXT" ("WAITING") ("CANCELLED"))
+                ("DONE" ("WAITING") ("CANCELLED"))
+                ("WAITING" ("WAITING" . t))
+                ("SOMEDAY" ("WAITING", t))
+                ("CANCELLED" ("CANCELLED" . t))
                 )))
   ;; logging
   (setq org-log-done 'time
@@ -83,47 +84,6 @@
   (setq org-hide-emphasis-markers nil
       org-ellipsis "▼")
 )
-
-;; lsp-mode
-(use-package! lsp-mode
-  :config
- ;; Support LSP in Org Babel with header argument `:file'.
-  ;; https://github.com/emacs-lsp/lsp-mode/issues/377
-  (defvar org-babel-lsp-explicit-lang-list
-    "Org Mode Babel languages which need explicitly specify header argument :file.")
-  (cl-defmacro lsp-org-babel-enbale (lang)
-    "Support LANG in org source code block."
-    ;; (cl-check-type lang symbolp)
-    (let* ((edit-pre (intern (format "org-babel-edit-prep:%s" lang)))
-           (intern-pre (intern (format "lsp--%s" (symbol-name edit-pre)))))
-      `(progn
-         (defun ,intern-pre (info)
-           (let ((lsp-file (or (->> info caddr (alist-get :file))
-                               buffer-file-name
-                               (unless (member ,lang org-babel-lsp-explicit-lang-list)
-                                 (concat (org-babel-temp-file (format "lsp-%s-" ,lang))
-                                         (cdr (assoc ,lang org-babel-tangle-lang-exts)))))))
-             (setq-local buffer-file-name lsp-file)
-             (setq-local lsp-buffer-uri (lsp--path-to-uri lsp-file))
-             (lsp)))
-         (if (fboundp ',edit-pre)
-             (advice-add ',edit-pre :after ',intern-pre)
-           (progn
-             (defun ,edit-pre (info)
-               (,intern-pre info))
-             (put ',edit-pre 'function-documentation
-                  (format "Add LSP info to Org source block dedicated buffer (%s)."
-                          (upcase ,lang))))))))
-
-  (defvar org-babel-lsp-lang-list
-    '("shell"
-      "python"
-      "ein"
-      "C" "C++"
-      "rust"))
-
-  (dolist (lang org-babel-lsp-lang-list)
-    (eval `(lsp-org-babel-enbale ,lang))))
 
 ; configure packages
 
