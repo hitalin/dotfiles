d10ac2bd43ed2bc96b8d8c1618f11fce4e36657b
hitalin <hitalin@users.noreply.github.com>
2020-06-01 13:38:13 +0900

    fix directory path

--- pwn/enhance_gdb.sh
+++ pwn/enhance_gdb.sh
@@ -2,8 +2,9 @@
 set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-mkdir ~/.gdb
-cd ~/.gdb
+if [ ! -d ~/.gdb/ ]; then
+  mkdir ~/.gdb
+fi
 
 if [ ! -d ~/.gdb/peda/ ]; then
   git clone https://github.com/longld/peda.git ~/.gdb/peda
@@ -13,8 +14,8 @@ if [ ! -d ~/.gdb/pwngdb/ ]; then
   git clone https://github.com/scwuaptx/Pwngdb.git ~/.gdb/pwngdb
 fi
 
-if [ ! -f ~/.gdbinit-gef.py ]; then
-  wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
+if [ ! -f ~/.gdb/.gdbinit-gef.py ]; then
+  wget -O ~/.gdb/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
   pip3 install --user capstone unicorn keystone-engine ropper
   git clone https://github.com/hugsy/gef-extras.git ~/.gdb/gef-extras
   gdb -ex 'gef config gef.extra_plugins_dir "~/.gdb/gef-extras/scripts"' -ex 'gef save' -ex quit
