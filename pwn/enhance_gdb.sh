1ce9e57da883b84b990fbd79a64afd2db5e659ba
hitalin <hitalin@users.noreply.github.com>
2020-06-01 13:34:16 +0900

    mkdir .gdb

--- pwn/enhance_gdb.sh
+++ pwn/enhance_gdb.sh
@@ -2,23 +2,26 @@
 set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-if [ ! -d ~/peda/ ]; then
-  git clone https://github.com/longld/peda.git ~/peda
+mkdir ~/.gdb
+cd ~/.gdb
+
+if [ ! -d ~/.gdb/peda/ ]; then
+  git clone https://github.com/longld/peda.git ~/.gdb/peda
 fi
 
-if [ ! -d ~/pwngdb/ ]; then
-  git clone https://github.com/scwuaptx/Pwngdb.git ~/pwngdb
+if [ ! -d ~/.gdb/pwngdb/ ]; then
+  git clone https://github.com/scwuaptx/Pwngdb.git ~/.gdb/pwngdb
 fi
 
 if [ ! -f ~/.gdbinit-gef.py ]; then
   wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
   pip3 install --user capstone unicorn keystone-engine ropper
-  git clone https://github.com/hugsy/gef-extras.git ~/gef-extras
-  gdb -ex 'gef config gef.extra_plugins_dir "~/gef-extras/scripts"' -ex 'gef save' -ex quit
+  git clone https://github.com/hugsy/gef-extras.git ~/.gdb/gef-extras
+  gdb -ex 'gef config gef.extra_plugins_dir "~/.gdb/gef-extras/scripts"' -ex 'gef save' -ex quit
 fi
 
-if [ ! -d ~/voltron/ ]; then
-  git clone https://github.com/snare/voltron.git ~/voltron
+if [ ! -d ~/.gdb/voltron/ ]; then
+  git clone https://github.com/snare/voltron.git ~/.gdb/voltron
 fi
 
 echo "Finished!"
