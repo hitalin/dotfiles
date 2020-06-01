0d1e8f9473d633e03afd8df3a875799e67cdc143
hitalin <hitalin@users.noreply.github.com>
2020-06-01 09:10:47 +0900

    add extra command of gef, and voltron UI

--- pwn/enhance_gdb.sh
+++ pwn/enhance_gdb.sh
@@ -12,10 +12,13 @@ fi
 
 if [ ! -d ~/.gdbinit-gef.py ]; then
   wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
-  pip3 install --user keystone-engine
-  pip3 install --user unicorn
-  pip3 install --user capstone
-  pip3 install --user ropper
+  pip3 install --user capstone unicorn keystone-engine ropper
+  git clone https://github.com/hugsy/gef-extras.git ~/gef-extras
+  gdb -ex 'gef config gef.extra_plugins_dir "~/gef-extras/scripts"' -ex 'gef save' -ex quit
+fi
+
+if [ ! -d ~/voltron/ ]; then
+  git clone https://github.com/snare/voltron.git
 fi
 
 echo "Finished!"
