5f6601db68fb46b9535e4faec6ee7acd8b7583ff
hitalin <hitalin@users.noreply.github.com>
2020-07-10 01:10:49 +0900

    update set.sh

--- set.sh
+++ set.sh
@@ -2,7 +2,7 @@
 set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-if [ ! -f ~/.config/nvim/ ]; then
+if [ ! -e ~/.config/nvim/ ]; then
   pip3 install --user pynvim neovim-remote
   mkdir -p ~/.config/nvim/
   ln -Fis $SCRIPT_DIR/.vimrc ~/.config/nvim/init.vim
@@ -12,20 +12,23 @@ if [ ! -f ~/.config/nvim/ ]; then
 #  ln -Fis $HOME/.vim/dein/repos/github.com/flazz/vim-colorschemes/colors/molokai.vim $HOME/.config/nvim/colors/molokai.vim
 fi
 
-if [ ! -f ~/.emacs.d/ ]; then
+if [ ! -e ~/.emacs.d/ ]; then
   mkdir -p ~/.emacs.d/
   git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+  ~/.emacs.d/bin/doom install
   mkdir -p ~/.doom.d
-  ln -Fis $SCRIPT_DIR/emacs/config.el ~/.doom.d/config.el
-  ln -Fis $SCRIPT_DIR/emacs/init.el ~/.doom.d/init.el
-  ln -Fis $SCRIPT_DIR/emacs/packages.el ~/.doom.d/packages.el
-  ln -Fis $SCRIPT_DIR/emacs/my-deft-title.el ~/.doom.d/my-deft-title.el
-  ln -Fis $SCRIPT_DIR/emacs/orgmode.el ~/.doom.d/orgmode.el
-  ln -Fis $SCRIPT_DIR/emacs/customs.el ~/.doom.d/customs.el
+  ln -fs $SCRIPT_DIR/emacs/config.el ~/.doom.d/config.el
+  ln -fs $SCRIPT_DIR/emacs/init.el ~/.doom.d/init.el
+  ln -fs $SCRIPT_DIR/emacs/packages.el ~/.doom.d/packages.el
+  ln -fs $SCRIPT_DIR/emacs/my-deft-title.el ~/.doom.d/my-deft-title.el
+  ln -fs $SCRIPT_DIR/emacs/orgmode.el ~/.doom.d/orgmode.el
+  ln -fs $SCRIPT_DIR/emacs/customs.el ~/.doom.d/customs.el
   ~/.emacs.d/bin/doom sync
   ~/.emacs.d/bin/doom doctor
 fi
 
-chsh -s $(which zsh)
+if [ -e $(which zsh) ]; then
+  chsh -s $(which zsh)
+fi
 
 echo "Finished!"
