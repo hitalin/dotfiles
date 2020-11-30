4cbd83ea8be1e57da77ddea9f07007003da9e768
hitalin <hitalin@users.noreply.github.com>
2020-11-30 21:56:17 +0900

    use symlinker.sh instead of ln.sh

--- ln.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-
-for file in `find $HOME/dotfiles -name '.*' | grep -v 'dotfiles/.git$' | perl -nle 'm!dotfiles/(.+)$! and print $1'`; do
-    ln -s -f $HOME/dotfiles/$file $HOME/$file
-done
-
-echo "Finished!"
