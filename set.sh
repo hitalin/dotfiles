e30326de002ec0c0a5cb8ebffa8d7b1786319105
hitalin <hitalin@users.noreply.github.com>
2021-03-27 22:43:46 +0900

    fix

--- set.sh
+++ set.sh
@@ -6,18 +6,13 @@ if [ -e $(which nvim) ] && [ ! -e ~/.config/nvim/ ]; then
   pip3 install --user pynvim neovim-remote
   mkdir -p ~/.config/nvim/
   ln -Fis $SCRIPT_DIR/.vimrc ~/.config/nvim/init.vim
-  mkdir -p ~/.config/nvim/colors
-  ln -Fis $HOME/.vim/dein/repos/github.com/flazz/vim-colorschemes/colors/molokai.vim $HOME/.config/nvim/colors/molokai.vim
+#  mkdir -p ~/.config/nvim/colors
+#  ln -Fis $HOME/.vim/dein/repos/github.com/flazz/vim-colorschemes/colors/molokai.vim $HOME/.config/nvim/colors/molokai.vim
 fi
 
 if [ -e $(which emacs) ] && [ ! -e ~/.emacs.d/ ]; then
   git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
   git clone https://github.com/i0z0m/.doom.d ~/.doom.d
-
-  git clone https://github.com/manateelazycat/emacs-application-framework.git --depth=1 ~/.emacs.d/site-lisp/emacs-application-framework/
-  chmod +x ~/.emacs.d/site-lisp/emacs-application-framework/install-eaf.sh
-  ~/.emacs.d/site-lisp/emacs-application-framework/install-eaf.sh
-
   ~/.emacs.d/bin/doom sync
   ~/.emacs.d/bin/doom doctor
 fi
