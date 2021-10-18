5e3d0ea1636241b92ce4fa78b8015f04b66cba01
hitalin <hitalin@users.noreply.github.com>
2021-10-19 02:15:19 +0900

    refile

--- set.sh
+++ set.sh
@@ -2,6 +2,17 @@
 set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+if [ -e $(which gdb) ] && [ ! -e ~/.gdb/ ]; then
+  mkdir -p ~/.gdb
+  git clone https://github.com/longld/peda.git ~/.gdb/peda
+  git clone https://github.com/scwuaptx/Pwngdb.git ~/.gdb/pwngdb
+
+  wget -O ~/.gdb/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
+  pip3 install --user rpyc capstone unicorn keystone-engine ropper
+  git clone https://github.com/hugsy/gef-extras.git ~/.gdb/gef-extras
+  gdb -ex 'gef config gef.extra_plugins_dir "~/.gdb/gef-extras/scripts"' -ex 'gef save' -ex quit
+fi
+
 if [ -e $(which nvim) ] && [ ! -e ~/.config/nvim/ ]; then
   pip3 install --user pynvim neovim-remote
   mkdir -p ~/.config/nvim/
