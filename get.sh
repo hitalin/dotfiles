1b9934ee1dc927d4e437c4e3d456dc189ff28817
hitalin <hitalin@users.noreply.github.com>
2021-10-18 23:02:51 +0900

    I have git

--- get.sh
+++ get.sh
@@ -1,21 +1,18 @@
 DOTPATH=~/dotfiles
 
-if has "git"; then
-    git clone --recursive "$GITHUB_URL" "$DOTPATH"
+if has "curl" || has "wget"; then
+  tarball="https://github.com/i0z0m/dotfiles/archive/master.tar.gz"
 
-elif has "curl" || has "wget"; then
-    tarball="https://github.com/i0z0m/dotfiles/archive/master.tar.gz"
+  if has "curl"; then
+    curl -L "$tarball"
 
-    if has "curl"; then
-        curl -L "$tarball"
+  elif has "wget"; then
+    wget -O - "$tarball"
 
-    elif has "wget"; then
-        wget -O - "$tarball"
+  fi | tar zxv
 
-    fi | tar zxv
-
-    mv -f dotfiles-master "$DOTPATH"
+  mv -f dotfiles-master "$DOTPATH"
 
 else
-    die "curl or wget required"
+  die "curl or wget required"
 fi
